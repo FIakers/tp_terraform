c5b040a458df4f7d27030b5782372b11b99079c1
FIakers <asainsardos@gmail.com>
2025-08-01 11:19:46 +0200

    Added default values for variables in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,18 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 variable "public_cidr" {
   description = "CIDR block for public subnet"
+  default     = "10.0.1.0/24"
 }
 variable "instance_type" {
   description = "EC2 instance type"
+  default     = "t2.micro"
 }
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
+  default     = "ami-08a6efd148b1f7504"
 }
 variable "key_name" {
   description = "Name for the SSH key"
