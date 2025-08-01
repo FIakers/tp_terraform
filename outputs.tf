1b33d0d4cde352d7120ec9d367aa0a348e2cce08
Clement Maroy <clement.maroy@gmail.com>
2025-08-01 11:34:43 +0200

    removed unused outputs

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,3 @@ output "ssh_connection_commands" {
   value = { for k, v in module.ec2_instance : k => "ssh -i ${local.workspace_prefix}-${var.key_name}.pem ec2-user@${v.public_ip}" }
   description = "SSH commands for all EC2 instances"
 }
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
-}
